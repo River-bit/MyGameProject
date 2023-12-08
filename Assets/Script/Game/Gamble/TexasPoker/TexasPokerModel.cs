e36ea20face0d2d3b8bf66d98627a65f5b605a89
unknown <moxiaochou123@hotmail.com>
2023-12-08 19:07:45 +0800

    poker

--- Assets/Script/Game/Gamble/TexasPoker/TexasPokerModel.cs
+++ Assets/Script/Game/Gamble/TexasPoker/TexasPokerModel.cs
@@ -2,7 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum CardType
+{
+    HighCard = 0,
+    OnePair = 1,
+    TwoPair = 2,
+    ThreeOfAKind = 3,
+    Straight = 4,
+    Flush = 5,
+    FullHouse = 6,
+    FourOfAKind = 7,
+    StraightFlush = 8,
+    RoyalFlush = 9
+}
+
 public class TexasPokerModel : BaseModel<TexasPokerModel>
 {
+    private bool[] _allCards = new bool[52];
+    private Dictionary<int, List<int>> _handCard = new Dictionary<int, List<int>>();
+    private List<bool> _fold = new List<bool>();
+    private int _step = 0;
+    private int _playerCnt = 2;
+    private int _dealerPos = 0;
 
+    public void InitGame(int playerCnt)
+    {
+        _step = 0;
+        
+    }
 }
